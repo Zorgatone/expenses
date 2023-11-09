581ced34bf13851e90f78d586adcafc1d243e542
Tommaso Ricci <zorgatone@gmail.com>
2023-11-09 21:31:42 +0100

    Cleanup

--- source/time_utils/time_utils.cpp
+++ source/time_utils/time_utils.cpp
@@ -6,25 +6,19 @@
 
 #include "time_utils.h"
 
-#ifdef __cplusplus
 extern "C" {
-#endif
+    // https://stackoverflow.com/a/75714048/1255826
+    std::time_t my_timegm(std::tm const *t) {
+        using namespace std::chrono;
+        return system_clock::to_time_t(
+                sys_days{year{t->tm_year+1900}/(t->tm_mon+1)/t->tm_mday} +
+                hours{t->tm_hour} + minutes{t->tm_min} + seconds{t->tm_sec});
+    }
 
-// https://stackoverflow.com/a/75714048/1255826
-std::time_t my_timegm(std::tm const *t) {
-    using namespace std::chrono;
-    return system_clock::to_time_t(
-            sys_days{year{t->tm_year+1900}/(t->tm_mon+1)/t->tm_mday} +
-            hours{t->tm_hour} + minutes{t->tm_min} + seconds{t->tm_sec});
+    std::time_t time_add(std::time_t t, long duration) {
+        using namespace std::chrono;
+        return system_clock::to_time_t(
+            system_clock::from_time_t(t) + seconds(duration)
+        );
+    }
 }
-
-std::time_t time_add(std::time_t t, long duration) {
-    using namespace std::chrono;
-    return system_clock::to_time_t(
-        system_clock::from_time_t(t) + seconds(duration)
-    );
-}
-
-#ifdef __cplusplus
-}
-#endif
